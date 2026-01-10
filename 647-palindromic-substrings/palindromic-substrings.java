d3a1d583ba33ce5333c5292e1dc766c5b5e41df1
Abhishek-Saraswat13 <saraswatabhishek9012553481@gmail.com>
2026-01-10 15:44:20 +0530

    Time: 21 ms (26.08%) | Memory: 46.4 MB (32.35%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,30 +1,24 @@
 class Solution {
-    int[][] t=new int[1001][1001];
     public int countSubstrings(String s) {
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            Arrays.fill(t[i],-1);
-        }
+        int n = s.length();
+        boolean[][] t = new boolean[n][n];
+        int c = 0;
 
-        int c=0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPalindrome(s,i,j)){
-                    c++;
+        for (int l = 1; l <= n; l++) {
+            for (int i = 0; i + l - 1 < n; i++) {
+                int j = i + l - 1;
+
+                if (i == j) {
+                    t[i][j] = true; // single char
+                } else if (i + 1 == j) {
+                    t[i][j] = (s.charAt(i) == s.charAt(j)); // length 2
+                } else {
+                    t[i][j] = (s.charAt(i) == s.charAt(j) && t[i + 1][j - 1]);
                 }
+
+                if (t[i][j]) c++;
             }
-        }                          
+        }
         return c;
-    }
-    public boolean isPalindrome(String s,int i,int j){
-       if(i>j) return true;
-       if(t[i][j]!=-1) return t[i][j]==1;
-       if(s.charAt(i)==s.charAt(j)){
-        boolean res=isPalindrome(s,i+1,j-1);
-        t[i][j]=res?1:0;
-        return res;
-       }
-       t[i][j]=0;
-       return false;
     }
 }

4fcebb07be4e5255c849140b99877243fdcef5eb
Abhishek-Saraswat13 <saraswatabhishek9012553481@gmail.com>
2026-01-10 09:36:37 +0530

    Time: 37 ms (10.23%) | Memory: 79 MB (8.70%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -4,30 +4,26 @@ class Solution {
     public int minDistance(String s1, String s2) {
         m=s1.length();
         n=s2.length();
-        for(int i=0;i<m;i++){
+        for(int i=0;i<=m;i++){
             Arrays.fill(dp[i],-1);
         }
 
-        return solve(s1,s2,0,0);
+        return solve(s1,s2,m,n);
     }
-    public int solve(String s1,String s2,int i,int j){
-        if(i>=m){
-            return n-j;
-        }
-        if(j>=n){
-            return m-i;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
-            dp[i][j]=solve(s1,s2,i+1,j+1);
+    public int solve(String s1,String s2,int m,int n){
+        if(m==0 || n==0) return m+n;
+        
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(s1.charAt(m-1)==s2.charAt(n-1)){
+            dp[m][n]=solve(s1,s2,m-1,n-1);
         }
         else{
-            int insert=1+solve(s1,s2,i,j+1);
-            int delete=1+solve(s1,s2,i+1,j);
-            int replace=1+solve(s1,s2,i+1,j+1);
+            int insert=1+solve(s1,s2,m,n-1);
+            int delete=1+solve(s1,s2,m-1,n);
+            int replace=1+solve(s1,s2,m-1,n-1);
 
-            return dp[i][j]=Math.min(Math.min(insert,delete),replace);
+            return dp[m][n]=Math.min(Math.min(insert,delete),replace);
         }
-        return dp[i][j];
+        return dp[m][n];
     }
 }

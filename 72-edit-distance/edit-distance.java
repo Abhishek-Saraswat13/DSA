5dfc4f022b13e3af5ff7764155975ca3c80997cd
Abhishek-Saraswat13 <saraswatabhishek9012553481@gmail.com>
2026-01-10 09:20:23 +0530

    Time: 37 ms (10.23%) | Memory: 78.4 MB (8.70%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -11,16 +11,15 @@ class Solution {
         return solve(s1,s2,0,0);
     }
     public int solve(String s1,String s2,int i,int j){
-        if(i==m){
+        if(i>=m){
             return n-j;
         }
-        else if(j==n){
+        if(j>=n){
             return m-i;
         }
         if(dp[i][j]!=-1) return dp[i][j];
-
         if(s1.charAt(i)==s2.charAt(j)){
-            return dp[i][j]=solve(s1,s2,i+1,j+1);
+            dp[i][j]=solve(s1,s2,i+1,j+1);
         }
         else{
             int insert=1+solve(s1,s2,i,j+1);
@@ -29,6 +28,6 @@ class Solution {
 
             return dp[i][j]=Math.min(Math.min(insert,delete),replace);
         }
-        
+        return dp[i][j];
     }
 }

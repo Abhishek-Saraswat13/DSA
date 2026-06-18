c9a653334560808da60125b4cd5877e9978db443
Abhishek-Saraswat13 <saraswatabhishek9012553481@gmail.com>
2026-06-18 11:13:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (81.39%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -3,15 +3,15 @@ class Solution {
         int n=nums.length;
         int l=0;
         int r=n-1;
+
         while(l<=r){
             int mid=l+(r-l)/2;
 
             if(nums[mid]==target){
                 return mid;
-
             }
             else if(nums[l]<=nums[mid]){
-                if(nums[l]<=target && nums[mid]>target){
+                if(nums[l] <= target && nums[mid] > target){
                     r=mid-1;
                 }
                 else{
@@ -25,7 +25,6 @@ class Solution {
                 else{
                     r=mid-1;
                 }
-
             }
         }
         return -1;
